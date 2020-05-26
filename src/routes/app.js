338d0f08ed936c4c613e9552527a604dceab4c11
Luiz <luizcarlos--1@hotmail.com>
2020-05-26 00:24:08 -0300

    node commits

---
+++ src/routes/app.js
@@ -0,0 +1,54 @@
+const { Router } = require('express');
+const router = Router();
+
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+router.post('/send-email', async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    contentHTML = `
+        <h1>User Information</h1>
+        <ul>
+            <li>Username: ${name}</li>
+            <li>User Email: ${email}</li>
+            <li>Subject: ${subject}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'yourmail@gmail.com',
+            pass: 'yourpassword'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+
+    // setup email data with unicode symbols
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Legolas" <yourmail@gmail.com>', // sender address,
+        to: 'yourmail@gmail.com',
+        subject: req.body.subject,
+        // text: 'Hello World'
+        html: contentHTML
+    })
+
+    console.log('Message sent: %s', info.messageId);
+
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.redirect('/success.html');
+});
+
+module.exports = router;
